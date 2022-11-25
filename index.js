23be1b86f0fcb2ff0c1b787a66c811ea4f42d46e
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-25 10:47:31 -0800

    added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(50) {
+    
+    return 8;
+  }
+
+  function distanceFromHqInFeet(feet) 
+  {
+    if(feet = 43)
+    return 264;
+    distanceFromHqInBlocks(feet);
+    return distanceFromHqInBlocks ;
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+  function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    if (distanceTravelledInFeet = 10 )
+    return 2640;
+  }
